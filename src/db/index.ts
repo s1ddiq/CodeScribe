21718e2bbf1d10b10b3ee651bfe55c9253fe9457
s1ddiq <siddiq.nasheed@gmail.com>
2025-07-17 19:14:16 +0300

    03: authentication setup

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,8 @@
 import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+config({ path: ".env" }); // or .env.local
+
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: sql });
